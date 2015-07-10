09b704fdb3fa130270b64c15c590f2db68667fe3
Rodrigo Morais <morais.rm@gmail.com>
2015-07-10 10:45:48 -0300

    Adding an example with multiple treatments to a promisse.

--- app/routes/first-three-repos.js
+++ app/routes/first-three-repos.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function(){
-        return Ember.$.getJSON('https://api.github.com/users/rodrigo-morais/repos').then(function(data){
+        var repos = Ember.$.getJSON('https://api.github.com/users/rodrigo-morais/repos');
+
+        var successOne = (data) => {
+            console.log('successOne: ' + data.length);
             return data.sort(function(previous, next){
                 if(previous.stargazers_count > next.stargazers_count){
                     return 1;
@@ -14,6 +17,26 @@ export default Ember.Route.extend({
                     return 0;
                 }
             }).slice(0, 3);
-        });
+        };
+
+        var successTwo = (data) => {
+            console.log('successTwo: ' + data.length);
+            return data;
+        };
+
+        var successThree = (data) => {
+            console.log('successThree: ' + data.length);
+            return data;
+        };
+
+        var reject = (reason) => {
+            console.log('reject: ' + reason);
+            return true;
+        };
+
+        return repos
+            .then(successOne)
+            .then(successTwo)
+            .then(successThree, reject);
     }
 });
