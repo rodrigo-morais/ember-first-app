da1a3d9f937fac6621ffc25162fd20b33b1564dd
Rodrigo Morais <morais.rm@gmail.com>
2015-07-17 17:29:10 -0300

    Improving the example of component.

--- app/components/text-color.js
+++ app/components/text-color.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
     text: '',
     actions: {
         changeColor: function(){
+            this.set('text', this.get('text') + ' clicked');
             if(Ember.$(this.get('element')).css('color') === 'rgb(0, 0, 0)'){
                 Ember.$(this.get('element')).css('color', '#00FF6A');
             }
