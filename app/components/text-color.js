35313a57d6b8cfb00d63dd7d992a7547eae21078
Rodrigo Morais <morais.rm@gmail.com>
2015-07-22 11:39:46 -0300

    Changing the text-color component to have an example of attributesBinding.

--- app/components/text-color.js
+++ app/components/text-color.js
@@ -1,14 +1,18 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
+    attributeBindings: 'color:data-color',
     text: '',
+    color: '#000000',
     actions: {
         changeColor: function(){
             this.set('text', this.get('text') + ' clicked');
             if(Ember.$(this.get('element')).css('color') === 'rgb(0, 0, 0)'){
+                this.set('color', '#00FF6A');
                 Ember.$(this.get('element')).css('color', '#00FF6A');
             }
             else{
+                this.set('color', '#000000');
                 Ember.$(this.get('element')).css('color', '#000000');
             }
         }
