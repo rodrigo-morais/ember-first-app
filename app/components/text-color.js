7e4c196c426a18ea1256dbe2120ff3d95b7a0d7a
Rodrigo Morais <morais.rm@gmail.com>
2015-07-23 07:05:06 -0300

    Adding a test of communication between component and application.

--- app/components/text-color.js
+++ app/components/text-color.js
@@ -6,15 +6,15 @@ export default Ember.Component.extend({
     color: '#000000',
     actions: {
         changeColor: function(){
+            let color = Ember.$(this.get('element')).css('color') === 'rgb(0, 0, 0)' ? '#00FF6A' : '#000000',
+                oppositeColor = color === '#000000' ? '#00FF6A' : "#000000";
+
             this.set('text', this.get('text') + ' clicked');
-            if(Ember.$(this.get('element')).css('color') === 'rgb(0, 0, 0)'){
-                this.set('color', '#00FF6A');
-                Ember.$(this.get('element')).css('color', '#00FF6A');
-            }
-            else{
-                this.set('color', '#000000');
-                Ember.$(this.get('element')).css('color', '#000000');
-            }
+            this.set('color', oppositeColor);
+            Ember.$(this.get('element')).css('color', color);
+
+
+            this.sendAction('changeColor', color);
         }
     }
 });
--- app/routes/simple-component.js
+++ app/routes/simple-component.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function(){
         return {title: 'route'};
+    },
+    actions: {
+        changeColor: function(color){
+            if(color === '#000000'){
+                Ember.$('body').css('background-color', "#FFFFFF");
+            }
+            else{
+                Ember.$('body').css('background-color', "#F5A300");
+            }
+        }
     }
 });
