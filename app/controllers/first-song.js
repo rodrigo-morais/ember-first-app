823e04cd3d2565b1c7e059d9be13f27aaa182539
Rodrigo Morais <morais.rm@gmail.com>
2015-07-28 09:24:38 -0300

    Adding a property in FirstSong controller to give an example between model property and controller property.

--- app/controllers/first-song.js
+++ app/controllers/first-song.js
@@ -1,5 +1,16 @@
 import Ember from "ember";
 
 export default Ember.ObjectController.extend({
-    
+    status: 'stop',
+    duration: function(){
+        var duration = this.get('model.duration'),
+            minutes = Math.floor(duration / 60),
+            seconds = duration % 60;
+
+            if(seconds.toString().length <= 1){
+                seconds = '0' + seconds;
+            }
+
+        return [minutes, seconds].join(':');
+    }.property('model.duration')
 });
