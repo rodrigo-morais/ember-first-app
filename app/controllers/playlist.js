c14bbe001cdf4fc83f038d0da95fe7f03a7e5cff
Rodrigo Morais <morais.rm@gmail.com>
2015-07-29 07:59:46 -0300

    Adding an example of property in ArrayController.

---
+++ app/controllers/playlist.js
@@ -0,0 +1,17 @@
+import Ember from "ember";
+
+export default Ember.ArrayController.extend({
+    duration: function(){
+        var total = this.get('model').reduce(function(previous, current){
+                        return previous + current.duration;
+                    }, 0),
+            minutes = Math.floor(total / 60),
+            seconds = total % 60;
+
+            if(seconds.toString().length <= 1){
+                seconds = '0' + seconds;
+            }
+
+        return [minutes, seconds].join(':');
+    }.property('model.@each.duration')
+});
